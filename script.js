8b1e84926f5cb7ff52d35387ef2a47157de33819
Pyrosublime <michiel.casey@gmail.com>
2023-03-22 17:29:49 +1000

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("hello 3"); // this is a comment
+/*
+console.log('hello 3');
+console.log('hello 3');
+console.log('hello 3');
+console.log('hello 3');
+console.log('hello 3');
+*/
+console.log(5+5);
+
+var a = "hello ";
+var b = "world";
+console.log(a + b);
